1accf4ea0a2e875305a99e2d754d1bf44bedad1f
Luk656 <miillerclei@hotmail.com>
2022-10-26 21:03:52 -0300

    alteração final com for

--- JS Web/aluramidi/main.js
+++ JS Web/aluramidi/main.js
@@ -1,3 +1,24 @@
+function tocaSom (idElementoAudio) {
+    document.querySelector(idElementoAudio).play();
+}
+
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+//para
+for (let contador = 0; contador < listaDeTeclas.length; contador++) {
+
+    const tecla = listaDeTeclas[contador];
+    const instrumento = tecla.classList[1];
+    const idAudio = `#som_${instrumento}`; //template string
+
+    tecla.onclick = function () {
+        tocaSom(idAudio);
+    }
+
+}
+
+
+/*
 function tocaSomPom (som) {
     document.querySelector(som).play();
 }
@@ -13,3 +34,5 @@ const listaDeSom = document.querySelectorAll('.som');
 listaDeteclas.forEach((lista, index)=> {
     lista.onclick = () => tocaSomPom('#'+listaDeSom[index].id);
 });
+
+*/
