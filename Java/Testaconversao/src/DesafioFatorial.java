8e7481e4b598e6be7b7b93b0c400253fd5c045cd
Luk656 <miillerclei@hotmail.com>
2022-10-30 23:09:42 -0300

    finalizando curso java basico

---
+++ Java/Testaconversao/src/DesafioFatorial.java
@@ -0,0 +1,21 @@
+
+/*public class DesafioFatorial {
+	public static void main(String[] args) {
+		for (int numero = 1; numero <= 6; numero++) {
+			for (int contador = 1; contador <= numero; contador++) {
+				System.out.println(numero + "fotorial e " + numero * contador);	
+			}
+		}
+	}
+}
+*/
+
+class DesafioFatorial {
+    public static void main(String[] args) {
+        int fatorial = 1;
+        for (int i = 1; i < 11; i++) {
+            fatorial *= i;
+            System.out.println("Fatorial de " + i + " = " + fatorial);
+        }
+    }
+}
---
+++ Java/Testaconversao/src/DesafioMultiplosDeTres.java
@@ -0,0 +1,15 @@
+
+public class DesafioMultiplosDeTres {
+	public static void main(String[] args) {
+		int numero = 0;
+		
+		while (numero < 100) {
+			if (numero % 3 == 0) {
+				System.out.print(numero + ", ");
+			}
+			numero++;
+		}
+
+		
+	}
+}
