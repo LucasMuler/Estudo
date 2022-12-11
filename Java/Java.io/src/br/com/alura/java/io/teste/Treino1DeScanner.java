1c0f8fc1b5442c23a3bbaceac5987553e40d0725
Luk656 <miillerclei@hotmail.com>
2022-12-11 00:22:39 -0300

    treino de Scanner

---
+++ Java/Java.io/src/br/com/alura/java/io/teste/Treino1DeScanner.java
@@ -0,0 +1,32 @@
+package br.com.alura.java.io.teste;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Treino1DeScanner {
+
+	public static void main(String[] args) throws IOException {
+		
+		Scanner scannerDeArquivo = new Scanner(new File("contas.csv"));		
+		
+		
+		while (scannerDeArquivo.hasNext()) {
+			String linha = scannerDeArquivo.nextLine();
+			System.out.println(linha); 
+	
+			Scanner scannerObjeto = new Scanner(linha);
+			scannerObjeto.useDelimiter(",");
+			
+			while (scannerObjeto.hasNext()) {
+				String obj = scannerObjeto.next();
+				System.out.println(obj);
+			}
+			
+			scannerObjeto.close();
+		}
+		scannerDeArquivo.close();
+	}
+	
+}
