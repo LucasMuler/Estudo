92a4f588c6d41c5ece624c820cd1f4104da44390
Luk656 <miillerclei@hotmail.com>
2023-02-07 20:04:53 -0300

    exercicio de Listas
    o exercicio proposto teria que ler quantos funcionários seriam cadastrados e qual ID de funcionário receberia um aumento

---
+++ Projetos_Java/EstudoUdemy/src/ExerciciosLista/Program.java
@@ -0,0 +1,62 @@
+package ExerciciosLista;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+import ExerciciosLista.Entities.Funcionario;
+
+public class Program {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("quantos funcionarios serã registrados?");
+		int numeroCadastro = sc.nextInt();
+		List<Funcionario> listaDeEmpregados = new ArrayList<>();
+		
+		for (int i = 0; i<numeroCadastro; i++) {
+			System.out.println("empregado numero #" + i);
+			System.out.println("Id: " );
+			int id = sc.nextInt();
+			sc.nextLine();
+			System.out.println("Nome: ");
+			String nome = sc.nextLine();
+			System.out.println("Salario: ");
+			double salario = sc.nextDouble();
+			sc.nextLine();
+			
+			Funcionario x = new Funcionario(id, nome, salario);
+			listaDeEmpregados.add(x);
+		}
+		
+		System.out.println("digite o id do funcionario que receberá um aumento");
+		int idDoAumento = sc.nextInt();
+	
+		for (Funcionario funcionario : listaDeEmpregados) {
+			if (funcionario.getId() == idDoAumento) {
+				System.out.println("Entre a porcentagem de aumento");
+				double porcentagemDoAumento = sc.nextDouble();
+				sc.nextLine();
+				
+				double aumento = (porcentagemDoAumento / 100) * funcionario.getSalario();
+				
+				System.out.println(aumento);
+				
+				funcionario.setSalario(aumento);
+			} else {
+				System.out.println("ID não registrado");
+			}
+		}
+		
+		System.out.println("Lista de funcionarios");
+		
+		for (Funcionario funcionario : listaDeEmpregados) {
+			System.out.println(funcionario);
+		}
+		
+		sc.close();
+	}
+
+}
--- Projetos_Java/EstudoUdemy/src/TesteDeListas.java
+++ Projetos_Java/EstudoUdemy/src/TesteDeListas.java
@@ -1,5 +1,6 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class TesteDeListas {
 
@@ -12,13 +13,41 @@ public class TesteDeListas {
 		minhaLista.add("Joao");
 		minhaLista.add("Sara");
 		minhaLista.add("Melo");
+		minhaLista.add("Cajamar");
 		
 		minhaLista.add(2, "Cleiton");
 		
 		for (String x : minhaLista) {
 			System.out.println(x);
 		}
+		
+		System.out.println("---------------------------------------");
+		
+		minhaLista.removeIf(x -> x.charAt(0) == 'M');
 
+		for (String x : minhaLista) {
+			System.out.println(x);
+		}
+		
+		System.out.println("-------------------------------------");
+		
+		System.out.println("index of Cleiton = " + minhaLista.indexOf("Cleiton"));
+		System.out.println("Index de um item que não existe = " + minhaLista.indexOf("Jose"));
+		
+		System.out.println("-------------------------------------");
+		
+		
+		List<String> result = minhaLista.stream().filter(x -> x.charAt(0) == 'C').collect(Collectors.toList());
+		for (String x : result) {
+			System.out.println(x);
+		}
+	
+		System.out.println("-------------------------------------");
+		
+		
+		String y = minhaLista.stream().filter(x -> x.charAt(0) == 'S').findFirst().orElse(null);
+		System.out.println(y);
+		
 	}
 
 }
