45c7471a41b8b5b9f63db44f6fd5744c98220bf9
Lucas Muller <miillerclei@hotmail.com>
2023-02-11 22:52:57 -0300

    Classe compost e StringBuilder
    programa simples que tem o objetivo de simular uma rede social

--- Projetos_Java/EstudoUdemy/src/ExercicioContrato/Entities/Contrato.java
+++ Projetos_Java/EstudoUdemy/src/ExercicioContrato/Entities/Contrato.java
@@ -39,7 +39,8 @@ public class Contrato {
 	}
 	
 	public double totalValue() {
-		return this.valorPorHora * this.horas;
+		double total = this.valorPorHora * this.horas;
+		return total;
 	}
 
 	@Override
---
+++ Projetos_Java/EstudoUdemy/src/ExercicioPost/Entities/Comment.java
@@ -0,0 +1,29 @@
+package ExercicioPost.Entities;
+
+public class Comment {
+
+	private String text;
+	
+	public Comment() {
+	}
+
+	public Comment(String text) {
+		this.text = text;
+	}
+
+	public String getText() {
+		return text;
+	}
+
+	public void setText(String text) {
+		this.text = text;
+	}
+
+	@Override
+	public String toString() {
+		return text;
+	}
+	
+	
+	
+}
