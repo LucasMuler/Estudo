ed05d9f5fb5fb32f8131dbf3ddab0ceb1e8735d9
Lucas Muller <miillerclei@hotmail.com>
2023-02-22 21:54:46 -0300

    Update Reserva
    O codigo anterior utilizava dois formatos de data, Date e Calendar, apos a atualização esta sendo utilizado apenas o Date

--- Projetos_Java/EstudoUdemy/src/ReservaHotelException/Entities/Reserva.java
+++ Projetos_Java/EstudoUdemy/src/ReservaHotelException/Entities/Reserva.java
@@ -2,7 +2,7 @@ package ReservaHotelException.Entities;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.util.Calendar;
+import java.util.Date;
 
 import ReservaHotelException.Exceptions.CheckInAfterCheckOutException;
 import ReservaHotelException.Exceptions.ReservDateAfterException;
@@ -12,17 +12,13 @@ public class Reserva {
 	SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 
 	private int numeroQuarto;
-	private String checkin;
-	private String checkout;
-
-	Calendar c1 = Calendar.getInstance();
-	Calendar c2 = Calendar.getInstance();
-	Calendar c3 = Calendar.getInstance();
+	private Date checkin;
+	private Date checkout;
 
 	public Reserva() {
 	}
 
-	public Reserva(int numeroQuarto, String checkin, String checkout) {
+	public Reserva(int numeroQuarto, Date checkin, Date checkout) {
 		this.numeroQuarto = numeroQuarto;
 		this.checkin = checkin;
 		this.checkout = checkout;
@@ -36,64 +32,51 @@ public class Reserva {
 		this.numeroQuarto = numeroQuarto;
 	}
 
+	@SuppressWarnings("deprecation")
 	public int duration() throws ParseException {
-		
-		c1.setTime(sdf.parse(this.checkin));
-		c2.setTime(sdf.parse(this.checkout));
-		
-		return c2.get(Calendar.DATE) - c1.get(Calendar.DATE);
-		
+
+		return checkout.getDay() - checkin.getDay();
+
 	}
 
-	public void updateDates(String checkin, String checkout) throws ParseException{
-		
-		c1.setTime(sdf.parse(this.checkin));
-		c2.setTime(sdf.parse(checkin));
-		c3.setTime(sdf.parse(checkout));
-		
-		if (c1.get(Calendar.DATE) <= c2.get(Calendar.DATE) && c1.get(Calendar.MONTH) <= c2.get(Calendar.MONTH) && 
-				c1.get(Calendar.YEAR) <= c2.get(Calendar.YEAR) && c2.get(Calendar.DATE) < c3.get(Calendar.DATE)) {
-			
+	public void updateDates(Date checkin, Date checkout) throws ParseException {
+
+		if (this.checkin.before(this.checkout) && this.checkin.before(checkin)) {
+
 			this.checkin = checkin;
 			this.checkout = checkout;
-			
-		} else if(c1.get(Calendar.DATE) > c2.get(Calendar.DATE)){
+
+		} else if (!this.checkin.before(checkin)) {
 			throw new ReservDateAfterException("Reservation dates for update must be future dates");
-			
-		} else if (c2.get(Calendar.DATE) > c3.get(Calendar.DATE)) {
+
+		} else if (!this.checkin.before(checkout)) {
 			throw new CheckInAfterCheckOutException("Check-out date must be after check-in date");
-			
+
 		}
-		
+
 	}
 
 	@Override
-	public String toString(){
+	public String toString() {
 
 		try {
-			
-			c1.setTime(sdf.parse(checkin));
-			c2.setTime(sdf.parse(checkout));
-			
-			if (c1.get(Calendar.DATE) <= c2.get(Calendar.DATE)) {
-				
-				return "Reserva numero Quarto = " + numeroQuarto + ", checkin = "+ checkin + ", checkout = "
-						 + checkout + ", " + duration() + " noites";
-				
-			} else if(c1.get(Calendar.DATE) > c2.get(Calendar.DATE)) {
-				
+
+			if (checkin.before(checkout)) {
+
+				return "Reserva numero Quarto = " + numeroQuarto + ", checkin = " + sdf.format(checkin)
+						+ ", checkout = " + sdf.format(checkout) + ", " + duration() + " noites";
+
+			} else {
+
 				throw new CheckInAfterCheckOutException("Check-out date must be after check-in date");
-				
+
 			}
 
 		} catch (ParseException e) {
-			
-			return "Parse Exception" + e.getMessage();
-			
-		} 
 
-		return "Reserva numeroQuarto=" + numeroQuarto + ", checkin=" + checkin + ", checkout=" + checkout;
+			return "Parse Exception" + e.getMessage();
+
+		}
 
 	}
-
 }
--- Projetos_Java/EstudoUdemy/src/ReservaHotelException/Program.java
+++ Projetos_Java/EstudoUdemy/src/ReservaHotelException/Program.java
@@ -1,7 +1,7 @@
 package ReservaHotelException;
 
 import java.text.ParseException;
-import java.util.Calendar;
+import java.text.SimpleDateFormat;
 import java.util.Scanner;
 
 import ReservaHotelException.Entities.Reserva;
@@ -13,45 +13,42 @@ public class Program {
 	public static void main(String[] args) throws ParseException {
 
 		Scanner sc = new Scanner(System.in);
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 
-		System.out.println("Numero do quarto:");
+		System.out.print("Numero do quarto: ");
 		int numero = sc.nextInt();
 		sc.nextLine();
 
-		System.out.println("Data de Checkin (dd/MM/yyyy):");
+		System.out.print("Data de Checkin (dd/MM/yyyy): ");
 		String checkin = sc.nextLine();
-		System.out.println("Date de Checkout (dd/MM/yyyy)");
+		System.out.print("Date de Checkout (dd/MM/yyyy): ");
 		String checkout = sc.nextLine();
 
 		try {
-			
-			Calendar c1 = Calendar.getInstance();
-			Calendar c2 = Calendar.getInstance();
-			
-			
-			
-			Reserva r = new Reserva(numero, checkin, checkout);
+
+			Reserva r = new Reserva(numero, sdf.parse(checkin), sdf.parse(checkout));
 
 			System.out.println(r);
 
 			System.out.println();
 			System.out.println("Entre a data para atualizar a reserva");
 			System.out.println();
-			System.out.println("Data de Checkin (dd/MM/yyyy):");
+			System.out.print("Data de Checkin (dd/MM/yyyy): ");
 			String checkinNovo = sc.nextLine();
-			System.out.println("Date de Checkout (dd/MM/yyyy)");
+			System.out.print("Date de Checkout (dd/MM/yyyy): ");
 			String checkoutNovo = sc.nextLine();
-			
-			r.updateDates(checkinNovo, checkoutNovo);
-			
+
+			r.updateDates(sdf.parse(checkinNovo), sdf.parse(checkoutNovo));
+
 			System.out.println(r);
 
 		} catch (CheckInAfterCheckOutException e) {
 			System.out.println("Error in reservation: " + e.getMessage());
 		} catch (ReservDateAfterException e) {
 			System.out.println("Error in reservation: " + e.getMessage());
+		} finally {
+			sc.close();	
 		}
-
 	}
 
 }
