4989b048d5973038e8b3e2d2a268b932bca4fa4f
Lucas Muller <miillerclei@hotmail.com>
2023-03-12 22:42:35 -0300

    File e Buffered Reader
    são duas formas não usuais de leitura de arquivos, como pode ser visto é um processo muito mais verboso e cansativo de ser interpretado

--- EstudoUdemy/src/LendoArquivoDeTexto/Program.java
+++ EstudoUdemy/src/LendoArquivoDeTexto/Program.java
@@ -8,16 +8,16 @@ public class Program {
 
 	public static void main(String[] args) {
 
-		File file = new File("c:\\temp\\in.txt");
+		File file = new File("c:\\temp\\in.txt"); //Cria um novo arquivo a apartir de um ja existente na maquina
 		Scanner sc = null;
 		try {
 
-			sc = new Scanner(file);
+			sc = new Scanner(file); // instancia o Scanner para ler o arquivo 
 			while (sc.hasNextLine()) {
-				System.out.println(sc.nextLine());
+				System.out.println(sc.nextLine()); // printa as proximas linhas
 			}
 
-		} catch (IOException e) {
+		} catch (IOException e) { // Exception mais comum de leitura de arquivos
 
 			System.out.println("Error: " + e.getMessage());
 
