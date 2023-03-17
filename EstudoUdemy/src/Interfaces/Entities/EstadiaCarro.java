1921e9b631ad313abefdae95360da7259e3d5662
Lucas Muller <miillerclei@hotmail.com>
2023-03-16 21:31:01 -0300

    Correção do código
    O mesmo anteriormente estava muito complexo e apenas funcionando o calculo das horas, foi feito uma alteração em que é calculado primeiramente quantos dias e depois o das horas

--- EstudoUdemy/src/Interfaces/Entities/EstadiaCarro.java
+++ EstudoUdemy/src/Interfaces/Entities/EstadiaCarro.java
@@ -1,18 +1,39 @@
 package Interfaces.Entities;
 
+import java.time.LocalDateTime;
 import java.util.Date;
 
 public class EstadiaCarro {
 
-	private Date comeco;
-	private Date fim;
+	private LocalDateTime comeco;
+	private LocalDateTime fim;
 	
-	public EstadiaCarro(Date comeco, Date fim) {
+	public EstadiaCarro(LocalDateTime comeco, LocalDateTime fim) {
 		this.comeco = comeco;
 		this.fim = fim;
-		
 	}
-	
-	
-	
+
+	public LocalDateTime getComeco() {
+		return comeco;
+	}
+
+	public void setComeco(LocalDateTime comeco) {
+		this.comeco = comeco;
+	}
+
+	public LocalDateTime getFim() {
+		return fim;
+	}
+
+	public void setFim(LocalDateTime fim) {
+		this.fim = fim;
+	}
+
+	@Override
+	public String toString() {
+		return "EstadiaCarro{" +
+				"comeco=" + comeco +
+				", fim=" + fim +
+				'}';
+	}
 }
--- EstudoUdemy/src/Interfaces/ProblemaSemInterface/Program.java
+++ EstudoUdemy/src/Interfaces/ProblemaSemInterface/Program.java
@@ -1,5 +1,8 @@
 package Interfaces.ProblemaSemInterface;
 
+import Interfaces.Entities.Carros;
+import Interfaces.Entities.EstadiaCarro;
+
 import java.text.ParseException;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
@@ -7,94 +10,104 @@ import java.time.temporal.ChronoUnit;
 import java.util.Scanner;
 
 public class Program {
-	public static void main(String[] args) throws ParseException {
-		DateTimeFormatter fmt = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm");
-		Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) throws ParseException {
+        DateTimeFormatter fmt = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm");
+        Scanner sc = new Scanner(System.in);
 
-		/**
-		 * ---------------------------------------------------------------------
-		 * data input
-		 */
-		System.out.println("Entre com os dados do aluguel");
-		System.out.print("Modelo do carro: ");
+        /**
+         * ---------------------------------------------------------------------
+         * data input
+         */
+        System.out.println("Entre com os dados do aluguel");
+        System.out.print("Modelo do carro: ");
 //		String model = sc.nextLine();
-		String model = "Civic";
-		System.out.println();
-		System.out.print("Retirada (dd/MM/yy HH:mm): ");
-//		String retirada = sc.nextLine();
-		String retirada = "25/06/2018 10:30";
-		System.out.println();
-		System.out.print("Entrada (dd/MM/yy HH:mm): ");
-//		String entrada = sc.nextLine();
-		String entrada = "27/06/2018 11:40";
-		System.out.println();
-		System.out.print("Entre com o preco da hora: ");
+        String model = "Civic";
+        System.out.println();
+        System.out.print("Retirada (dd/MM/yy HH:mm): ");
+//		String out = sc.nextLine();
+        String out = "25/06/2018 10:30";
+        System.out.println();
+        System.out.print("Entrada (dd/MM/yy HH:mm): ");
+//		String in = sc.nextLine();
+        String in = "25/06/2018 14:40";
+        System.out.println();
+        System.out.print("Entre com o preco da hora: ");
 //		double precoHora = sc.nextDouble();
 //		sc.nextLine();
-		double precoHora = 10.00;
-		System.out.println();
-		System.out.print("Entre com o preco dia: ");
+        double precoHora = 10.00;
+        System.out.println();
+        System.out.print("Entre com o preco dia: ");
 //		double precoDia = sc.nextDouble();
 //		sc.nextLine();
-		double precoDia = 130.00;
-		System.out.println();
-		
-		/**
-		 * ---------------------------------------------------------------------
-		 * Logic of the main code
-		 */
-		
-		LocalDateTime comeco = LocalDateTime.parse(retirada, fmt);
-		LocalDateTime fim = LocalDateTime.parse(entrada, fmt);
-		double periodoMinutos = comeco.until(fim, ChronoUnit.MINUTES);
-		double periodoHoras = comeco.until(fim, ChronoUnit.HOURS);
-		double periodoDias = comeco.until(fim, ChronoUnit.DAYS);
-		double total = 0;
-		int horas = 0;
-		int dias = 0;
-		
-		if (periodoDias >= 1) {
-			
-			total += precoDia * (int)(periodoDias);
-			
-			if (periodoMinutos > 0) {
-				if (periodoMinutos < 720) {
-					total += calculoHora(periodoMinutos, periodoHoras, precoHora);
-				} else {
-					total += calculoHora(periodoMinutos, periodoHoras, precoHora) + 130;
-				}
-			} 
-			
-		} else {
-			
-			if (periodoMinutos > 0) {
-				if (periodoMinutos < 720) {
-					total += calculoHora(periodoMinutos, periodoHoras, precoHora);
-				} else {
-					total += calculoHora(periodoMinutos, periodoHoras, precoHora) + 130;
-				}
-			} 
-			
-		} 
-		
-		
-		/**
-		 * ---------------------------------------------------------------------
-		 * data output
-		 */
-		System.out.println(periodoMinutos);
-		System.out.println(periodoHoras);
-		System.out.println(periodoDias);
-		System.out.println(total);
-		sc.close();
-	}
-	
-	public static double calculoHora(double periodoMinutos, double periodoHoras, double precoHora) {
-		if(periodoMinutos % 60 > 0) {
-			return (int)((periodoHoras) + 1) * precoHora;
-		} else {
-			return  (int)(periodoHoras) * precoHora;
-		}
-	}
-	
+        double precoDia = 130.00;
+        System.out.println();
+
+        /**
+         * ---------------------------------------------------------------------
+         * Logic of the main code
+         */
+
+        LocalDateTime comeco = LocalDateTime.parse(out, fmt);
+        LocalDateTime fim = LocalDateTime.parse(in, fmt);
+
+        double periodoMinutos = comeco.until(fim, ChronoUnit.MINUTES);
+        double periodoHoras = 0;
+        double periodoDias = 0;
+        double total = 0;
+        int umDiaEmMinutos = 1440;
+
+        Carros c = new Carros(model);
+        EstadiaCarro ec = new EstadiaCarro(comeco,fim);
+
+        /**
+         * Logica de calculo de estadia
+         */
+
+        while(periodoMinutos > 0 && periodoMinutos > 740){
+            periodoMinutos -= 740;
+            periodoDias += 1;
+
+            if (periodoMinutos >= 740){
+                periodoMinutos -= 740;
+            }
+
+        }
+
+        if (periodoDias >= 1 && periodoMinutos < 740){
+            periodoDias += 1;
+        }
+        else {
+            int aux = 740;
+            if (periodoMinutos - 740 <= 0){
+                total += calculoHora(periodoMinutos, precoHora);
+            } else{
+                periodoMinutos -= 740;
+            }
+        }
+
+        total += precoDia * (int) (periodoDias);
+
+        /**
+         * ---------------------------------------------------------------------
+         * data output
+         */
+        System.out.println(periodoMinutos);
+        System.out.println(periodoHoras);
+        System.out.println(periodoDias);
+        System.out.println(total);
+        sc.close();
+    }
+
+    public static double calculoHora(double periodoMinutos, double precoHora) {
+
+        int periodoHoras = (int) (periodoMinutos / 60);
+        periodoMinutos -= periodoHoras * 60;
+
+        if (periodoMinutos % 60 > 0) {
+            return (int) ((periodoHoras) + 1) * precoHora;
+        } else {
+            return (int) (periodoHoras) * precoHora;
+        }
+    }
+
 }
