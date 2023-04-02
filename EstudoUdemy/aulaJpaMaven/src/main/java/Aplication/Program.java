dd9c0bfd9bbbf61fc91664a493dacb94355fd32f
Luk656 <miillerclei@hotmail.com>
2023-04-02 12:11:55 -0300

    Buscando pessoa

--- EstudoUdemy/aulaJpaMaven/src/main/java/Aplication/Program.java
+++ EstudoUdemy/aulaJpaMaven/src/main/java/Aplication/Program.java
@@ -9,18 +9,14 @@ import javax.persistence.Persistence;
 public class Program {
     public static void main(String[] args) {
 
-        Pessoa p1 = new Pessoa(0, "Carlos da Silva", "carlos@gmail.com");
-        Pessoa p2 = new Pessoa(0, "Joaquim Torres", "joaquim@gmail.com");
-        Pessoa p3 = new Pessoa(0, "Ana Maria", "ana@gmail.com");
-
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("exemplo-jpa");
         EntityManager em = emf.createEntityManager();
 
-        em.getTransaction().begin();
-        em.persist(p1);
-        em.persist(p2);
-        em.persist(p3);
-        em.getTransaction().commit();
+        Pessoa p = em.find(Pessoa.class, 2);
+        System.out.println(p);
+
         System.out.println("pronto");
+        em.close();
+        emf.close();
     }
 }
