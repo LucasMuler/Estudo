320cc7ae72758df3ff667637c63b808e99e2788b
Lucas Muller <miillerclei@hotmail.com>
2023-04-18 21:42:38 -0300

    Alterando a listagem e ordenação
    O springBoot tem uma ordenação e listagem padrão, porem como mostra esta anotação é possivel altera-la, lembrando que esta condição so vai ser executada caso não tenha nada na URL

--- EstudoUdemy/ProjetosSpring/apiValMed/src/main/java/med/vol/api/controller/MedicoController.java
+++ EstudoUdemy/ProjetosSpring/apiValMed/src/main/java/med/vol/api/controller/MedicoController.java
@@ -8,6 +8,7 @@ import med.vol.api.entitie.medico.medico;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
@@ -27,7 +28,7 @@ public class MedicoController {
     }
 
     @GetMapping
-    public Page<dadosListagemMedico> listar(Pageable paginacao){
+    public Page<dadosListagemMedico> listar(@PageableDefault(size = 10, sort = {"nome"}) Pageable paginacao){
         return repository.findAll(paginacao).map(dadosListagemMedico::new);
     }
 
