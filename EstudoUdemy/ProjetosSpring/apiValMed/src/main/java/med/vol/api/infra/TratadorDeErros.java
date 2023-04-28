dbd1f446466bbb21f56b6815125912696985c13a
Lucas Muller <miillerclei@hotmail.com>
2023-04-27 21:06:43 -0300

    Tratando Erro 400
    Neste ponto de tratar o erro 400 ocorreu um erro inesperado com o da aula, pois o meu trazia o erro 500, ao olhar o forum um rapaz sugeriu fazer esta outra tratativa direcionada a Exception que foi capturada, e o mesmo funcionou corretamente

--- EstudoUdemy/ProjetosSpring/apiValMed/src/main/java/med/vol/api/infra/TratadorDeErros.java
+++ EstudoUdemy/ProjetosSpring/apiValMed/src/main/java/med/vol/api/infra/TratadorDeErros.java
@@ -2,8 +2,13 @@ package med.vol.api.infra;
 
 import jakarta.persistence.EntityNotFoundException;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import javax.naming.directory.InvalidAttributesException;
+import java.sql.SQLIntegrityConstraintViolationException;
 
 @RestControllerAdvice
 public class TratadorDeErros {
@@ -13,4 +18,28 @@ public class TratadorDeErros {
         return ResponseEntity.notFound().build();
     }
 
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity tratarErro400(MethodArgumentNotValidException e){
+        var erros = e.getFieldErrors();
+        return ResponseEntity.badRequest().body(erros.stream().map(DadosErrosValidacao::new).toList());
+    }
+
+    @ExceptionHandler(SQLIntegrityConstraintViolationException.class)
+    public ResponseEntity tratarErro400ParaRegistroDuplicado() {
+        return ResponseEntity.badRequest().body("Registro já cadastrado!");
+    }
+
+
+    /*
+    ----------------------DTO para pegar os campos de nome do erro e a mensagem que cada um recebe------------------
+     */
+    private record DadosErrosValidacao(String campo, String mensagem){
+
+        public DadosErrosValidacao(FieldError erro){
+            this(erro.getField(), erro.getDefaultMessage());
+        }
+    }
+
 }
+
+
