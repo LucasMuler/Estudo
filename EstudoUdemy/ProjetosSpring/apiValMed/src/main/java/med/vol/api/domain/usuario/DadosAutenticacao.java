e2aaa5d870adc431012c7ca4ec42bd41e0be0143
Luk656 <miillerclei@hotmail.com>
2023-05-03 19:09:22 -0300

    adicionando controller de Usuario

---
+++ EstudoUdemy/ProjetosSpring/apiValMed/src/main/java/med/vol/api/domain/usuario/DadosAutenticacao.java
@@ -0,0 +1,11 @@
+package med.vol.api.domain.usuario;
+
+import jakarta.validation.constraints.NotBlank;
+
+public record DadosAutenticacao(
+        @NotBlank
+        String login,
+
+        @NotBlank
+        String senha) {
+}
