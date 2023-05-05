d5cefc7b21c4dcdaa5eaefc3eede256373dc9689
Luk656 <miillerclei@hotmail.com>
2023-05-05 13:29:00 -0300

    alterando inicio dos filtros

--- EstudoUdemy/ProjetosSpring/apiValMed/src/main/java/med/vol/api/infra/security/Securityconfigurations.java
+++ EstudoUdemy/ProjetosSpring/apiValMed/src/main/java/med/vol/api/infra/security/Securityconfigurations.java
@@ -1,5 +1,6 @@
 package med.vol.api.infra.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -11,10 +12,14 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 public class Securityconfigurations {
+
+    @Autowired
+    private SecurityFilter filter;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -23,7 +28,8 @@ public class Securityconfigurations {
                 .and().authorizeHttpRequests()
                 .requestMatchers(HttpMethod.POST, "/login").permitAll()
                 .anyRequest().authenticated()
-                .and().build();
+                .and().addFilterBefore(filter, UsernamePasswordAuthenticationFilter.class)
+                .build();
     }
 
     @Bean
