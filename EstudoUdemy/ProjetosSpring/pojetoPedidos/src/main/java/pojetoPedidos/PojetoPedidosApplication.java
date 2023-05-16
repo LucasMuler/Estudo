167746dddecaf57495cfa2f60e463c795963abb9
Lucas Muller <miillerclei@hotmail.com>
2023-05-15 23:39:32 -0300

    Base do projeto
    Este será um projeto de pedidos, onde será utilizado apenas o devtools e lombook

---
+++ EstudoUdemy/ProjetosSpring/pojetoPedidos/src/main/java/pojetoPedidos/PojetoPedidosApplication.java
@@ -0,0 +1,13 @@
+package pojetoPedidos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PojetoPedidosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PojetoPedidosApplication.class, args);
+	}
+
+}
---
+++ EstudoUdemy/ProjetosSpring/pojetoPedidos/src/test/java/pojetoPedidos/PojetoPedidosApplicationTests.java
@@ -0,0 +1,13 @@
+package pojetoPedidos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PojetoPedidosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
